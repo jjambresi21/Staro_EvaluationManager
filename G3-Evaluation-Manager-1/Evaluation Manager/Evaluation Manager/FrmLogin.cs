651c29ce394db5ac49950facac55e5ff8bfc807f
jjambresi21 <126493519+jjambresi21@users.noreply.github.com>
2023-05-08 16:26:33 +0200

    Add files via upload

---
+++ G3-Evaluation-Manager-1/Evaluation Manager/Evaluation Manager/FrmLogin.cs
@@ -0,0 +1,39 @@
+﻿using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager
+{
+    public partial class FrmLogin : Form
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (username == "nastavnik" && password == "test")
+            {
+                FrmStudents frmStudents = new FrmStudents();
+                Hide();
+                frmStudents.ShowDialog();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Neispravni podaci");
+            }
+        }
+    }
+}
---
+++ G3-Evaluation-Manager-1/Evaluation Manager/Evaluation Manager/FrmStudents.cs
@@ -0,0 +1,36 @@
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluation_Manager
+{
+    public partial class FrmStudents : Form
+    {
+        public FrmStudents()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmStudents_Load(object sender, EventArgs e)
+        {
+            dgvStudents.DataSource = StudentRepository.GetStudents();
+        }
+
+        private void btnEvaluateStudent_Click(object sender, EventArgs e) {
+            Student student = dgvStudents.CurrentRow.DataBoundItem as Student;
+            if (student != null) {
+                var FrmEvaluation = new FrmEvaluation(student);
+                FrmEvaluation.ShowDialog();
+            }
+
+        }
+    }
+}
